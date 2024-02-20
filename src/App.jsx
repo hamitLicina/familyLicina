cab678261387e10136305ac2eeba39458ea60e62
hamitLicina <hamitlicina@gmail.com>
2024-02-20 21:06:54 +0300

    Important folders added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
 import './App.css'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import Category from './pages/Category/Category'
+
 
 function App() {
 
 
   return (
-    <>
-      <>
-        
-      </>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/category/:categoryName' element={<Category/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
