07da3cbe84ac4c456c4bb9dcf176e514fb84c66e
hamitLicina <hamitlicina@gmail.com>
2024-02-23 21:03:38 +0300

    Home icon navigate to the Home Page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import './Header.css'
 import { FaHome } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
 
   const categories = ["Family Members", "Travel", "Health", "Food"]
 
+  const navigate = useNavigate()
+
 
   return (
     <div className='header-container'>
-      <FaHome />
+      <FaHome onClick={() => navigate('/')} className='home-icon'/>
       <div className='categories-container'>
         {
           categories.map((item) => (<Link className='nav-link' to={`/category/${item}`}>{item}</Link>))
