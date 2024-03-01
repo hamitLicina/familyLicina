6b7d7e7742c85e1360a0ceee6fb71d329e997443
hamitLicina <hamitlicina@gmail.com>
2024-03-01 15:28:56 +0300

    Category part finished and I need to add Some data to FireStore

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import './Category.css'
+import React, { useEffect, useState } from "react";
+import "./Category.css";
+import { useParams } from "react-router-dom";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../config/FirebaseConfig";
 
 function Category() {
+  const { categoryName } = useParams();
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    // Create a reference to firebase db collection
+    const articleRef = collection(db, "FamilyMember");
+    // Now Create query
+    const q = query(articleRef, where("category", "==", categoryName));
+    // Now get data that matches the query
+    getDocs(q, articleRef)
+      .then((res) => {
+        const articles = res.docs.map((item) => ({
+          ...item.data(),
+          id: item.id,
+        }));
+        setArticles(articles);
+      })
+      .catch((err) => console.log(err));
+  }, [categoryName]);
   return (
-    <div>Category</div>
-  )
+    <div>
+      {articles.map((item) => (
+        <h2>{item?.title}</h2>
+      ))}
+    </div>
+  );
 }
 
-export default Category
+export default Category;
