430d9f73e32e4d5461fb0ea29058a0650b63a81f
hamitLicina <hamitlicina@gmail.com>
2024-03-08 22:16:15 +0300

    Family Member add page

---
+++ src/pages/AddFamilyMember/AddFamilyMember.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './AddFamilyMember.css'
+
+// There is lots of work I need to continue tomorrow 
+
+function AddFamilyMember() {
+  return (
+    <div>
+        // I need to convert this page to Add Family Member
+    </div>
+  )
+}
+
+export default AddFamilyMember
